2e48b907e9cee0e7c3f6adefb5626a2bba73a2d9
kacperKwiatkowski <kacper18k@gmail.com>
2023-01-30 10:25:35 +0100

    Player and obstacle added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
     private Rigidbody playerRb;
     
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        playerRb = GetComponent<Rigidbody>(); 
+        Physics.gravity *= gravityModifier;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
